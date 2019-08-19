c971fbd9be16e00fe161cbca5ca40f7d212aa022
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2019-08-19 16:06:47 -0300

    Start conection to twitter API
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

---
+++ example_tokens.py
@@ -0,0 +1,5 @@
+# You must creat a twitter developpen accont and an app that has an API token and a API secret token, fill the blanks bellow and change the name of the file for tokens.py
+
+
+API_TOKEN = 'Your App API token'
+API_TOKEN_SECRET = 'Your API secret TOKEN'
---
+++ twitterAPI.py
@@ -0,0 +1,36 @@
+import requests
+import json
+from tokens import BAREAR_TOKEN, API_TOKEN, API_TOKEN_SECRET
+import base64
+import urllib.parse
+
+TOKEN = ''
+
+def create_barear_token():
+    global TOKEN
+    OAUTH2_TOKEN = 'https://api.twitter.com/oauth2/token'
+    consumer_key = urllib.parse.quote(API_TOKEN)
+    consumer_secret = urllib.parse.quote(API_TOKEN_SECRET)
+    bearer_token = consumer_key + ':' + consumer_secret
+    base64_encoded_bearer_token = base64.b64encode(bearer_token.encode('utf-8'))
+    headers = {
+        "Authorization": "Basic " + base64_encoded_bearer_token.decode('utf-8') + "",
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+        "Content-Length": "29"}
+
+    response = requests.post(OAUTH2_TOKEN, headers=headers, data={'grant_type': 'client_credentials'})
+    to_json = response.json()
+    TOKEN = to_json['access_token']
+
+
+def get_user_id(username):
+    header = {'authorization': 'Bearer ' + BAREAR_TOKEN}
+    url = 'https://api.twitter.com/1.1/users/show.json?screen_name=' + username
+    r = requests.get(url, headers=header)
+    user = json.loads(r.text)
+    print(user['name'])
+    print(user['id'])
+
+
+create_barear_token()
+print(TOKEN)
